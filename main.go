fb8ca1b19e4d63eb4567ba6124307867f8de1bf6
repositoryplumber <satpidepik@proton.me>
2024-07-31 11:19:20 +0800

    Go ~ Main Commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"github.com/gorilla/mux"
+	"fmt"
+)
+
+// New Struct
+type main struct {
+	HW	string `Hello, World!`
+}
+
+func main() {
+	fmt.println(HW)
+	// Some print statements
+	fmt.println("This is EPIC!!!")
+}
+
+
+
